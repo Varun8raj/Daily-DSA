2ea8884c542576f042bc5713cea5081db3d5a3e3
Varun8raj <rajvarun.tenugu@gmail.com>
2026-02-14 12:18:21 -0500

    add 1 easy problem

--- Basics/BasicMaths/CountOddDigits.java
+++ Basics/BasicMaths/CountOddDigits.java
@@ -1,17 +1,18 @@
-/* Approach : We can get the number of digits by reducing the number/10 each time, until the number gets to 0 */
+/* Approach : We can get the number of odd digits by getting the last digit each time and checking if it odd, until the number gets to 0 */
 // Time Complexity = O(log10(n))
 // Space Complexity = O(1)
 package Basics.BasicMaths;
 
 class Solution {
-    public int countDigit(int n) {
+    public int countOddDigit(int n) {
         int num = n;
         int count = 0;
         if (num == 0)
-            return 1;
+            return 0;
         while (num > 0) {
+            if ((num % 10) % 2 == 1)
+                count++;
             num = num / 10;
-            count = count + 1;
         }
         return count;
     }
