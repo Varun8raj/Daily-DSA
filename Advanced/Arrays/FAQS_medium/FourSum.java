6e98df480dc1997ec1d96674dd9eadbc7073748b
Varun8raj <rajvarun.tenugu@gmail.com>
2026-03-05 16:30:53 -0500

    add 2 medium problems

---
+++ Advanced/Arrays/FAQS_medium/FourSum.java
@@ -0,0 +1,49 @@
+/* Approach : The approach for this problem is to first sort the give array and then use the 4 pointers using the concept of 3 pointer(3Sum) to keep track and find the unique sets of the triplets*/
+// Time Complexity = O(nlogn + n^3)
+// Space Complexity = O(1)
+
+package Advanced.Arrays.FAQS_medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if (sum == target) {
+                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1])
+                            k++;
+                        while (k < l && nums[l] == nums[l + 1])
+                            l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
---
+++ Advanced/Arrays/FAQS_medium/ThreeSum.java
@@ -0,0 +1,48 @@
+/* Approach : The approach for this problem is to first sort the give array and then use the 3 pointers using the concept of 2 pointer to keep track and find the unique sets of the triplets*/
+// Time Complexity = O(nlogn + n^2)
+// Space Complexity = O(1)
+
+package Advanced.Arrays.FAQS_medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
