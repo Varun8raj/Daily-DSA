1f69cec81df7547bb86c0a8bd2809f4c1af63c0b
Varun8raj <rajvarun.tenugu@gmail.com>
2026-03-09 14:55:45 -0400

    add 1 hard problem

---
+++ Advanced/Arrays/FAQS_Hard/MajorityElementII.java
@@ -0,0 +1,57 @@
+/* Approach : We can approach this problem by using the boyer-moore's voting algorithm, we keep track of two major element's count by traversing through the array, and if an element is different from the tracking element we reduce the count, and if it is zero then we just change the tracking element to the new one. */
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+package Advanced.Arrays.FAQS_Hard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> majorityElementTwo(int[] nums) {
+        int n = nums.length;
+        int cnt1 = 0, cnt2 = 0;
+
+        int el1 = Integer.MIN_VALUE, el2 = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && el2 != nums[i]) {
+                cnt1 = 1;
+                el1 = nums[i];
+            } else if (cnt2 == 0 && el1 != nums[i]) {
+                cnt2 = 1;
+                el2 = nums[i];
+            } else if (nums[i] == el1) {
+                cnt1++;
+            } else if (nums[i] == el2) {
+                cnt2++;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1 = 0;
+        cnt2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el1) {
+                cnt1++;
+            }
+            if (nums[i] == el2) {
+                cnt2++;
+            }
+        }
+
+        int mini = n / 3 + 1;
+        List<Integer> result = new ArrayList<>();
+
+        if (cnt1 >= mini) {
+            result.add(el1);
+        }
+        if (cnt2 >= mini && el1 != el2) {
+            result.add(el2);
+        }
+        return result;
+    }
+}
